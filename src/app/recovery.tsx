49d49873372eabf0339b9c85b27c714c2cbe046a
antony-souza <antonygustavo09@hotmail.com>
2025-01-23 00:12:28 -0300

    Crindo steps de recovery

--- src/app/recovery.tsx
+++ src/app/recovery.tsx
@@ -1,119 +1,162 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Control, Controller, useForm } from "react-hook-form";
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import Icon from "react-native-vector-icons/FontAwesome5";
 import { environment } from "../../environment/environment";
-import { Link } from "expo-router";
 import { httpClient } from "../../utils/generic-request";
 import { InputCase } from "../../components/input";
+import { Link } from "expo-router";
 
 interface IAuth {
-    email: string;
-    password: string;
+  email: string;
+  code: string;
+  password: string;
 }
 
 export default function RecoveryPassword() {
+  const [step, setStep] = useState(1); 
+  const { control, handleSubmit, formState: { isValid } } = useForm<IAuth>({ mode: "onChange" });
 
-    const { control, handleSubmit, formState: { isValid } } = useForm<IAuth>({
-        mode: "onChange"
-    });
+  const handleEmailSubmit = async (data: { email: string }) => {
+    /* await httpClient.genericRequest(`${environment.auth}/send-code`, "POST", { email: data.email }); */
+    setStep(2); 
+  };
 
-    const onSubmit = async (data: IAuth) => {
-        return await httpClient.genericRequest(environment.auth, "POST", data);
-    };
+  const handleCodeSubmit = async (data: { code: string }) => {
+    /* await httpClient.genericRequest(`${environment.auth}/verify-code`, "POST", { code: data.code }); */
+    setStep(3);
+  };
 
+  const handlePasswordSubmit = async (data: { password: string }) => {
+    /* await httpClient.genericRequest(`${environment.auth}/update-password`, "POST", { password: data.password }); */
+    alert("Senha atualizada com sucesso!");
+  };
 
-    return (
-        <View style={styles.logoContainer}>
-            <Icon
-                name="lock"
-                size={60}
-                color="white"
-                style={{ marginBottom: 20 }}
+  return (
+    <View style={styles.container}>
+      <Icon name="lock" size={60} color="white" style={{ marginBottom: 20 }} />
+
+      {step === 1 && (
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: "Email é obrigatório",
+            pattern: {
+              value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              message: "Email inválido",
+            },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <InputCase
+              icon="envelope"
+              placeholder="Digite seu email"
+              value={value}
+              onChange={onChange}
+              keyboardType="email-address"
+              />
+          )}
+        />
+      )}
+
+      {step === 2 && (
+        <Controller
+          control={control}
+          name="code"
+          rules={{ required: "Código é obrigatório" }}
+          render={({ field: { onChange, value } }) => (
+            <InputCase
+              icon="key"
+              placeholder="Digite o código de verificação"
+              value={value}
+              onChange={onChange}
+              keyboardType="numeric"
             />
+          )}
+        />
+      )}
 
-            <Controller
-                control={control}
-                name="email"
-                rules={{
-                    required: "Email é obrigatório",
-                    pattern: {
-                        value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-                        message: "Email inválido"
-                    }
-                }}
-                render={({ field: { onChange, value } }) => (
-                    <InputCase
-                        icon="envelope"
-                        placeholder="Digite o email de recuperação"
-                        value={value}
-                        onChange={onChange}
-                        keyboardType="email-address"
-                    />
-                )}
+      {step === 3 && (
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required: "Senha é obrigatória",
+            minLength: { value: 6, message: "Senha deve ter pelo menos 6 caracteres" },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <InputCase
+              icon="lock"
+              placeholder="Digite sua nova senha"
+              value={value}
+              onChange={onChange}
+              secureTextEntry
             />
-            <TouchableOpacity
-                style={[styles.button, !isValid ? { backgroundColor: "#888" } : {}]}
-                onPress={handleSubmit(onSubmit)}
-                disabled={!isValid}
-            >
-                <Text style={styles.buttonText}>Enviar</Text>
-            </TouchableOpacity>
-            <View style={styles.button}>
-                <Link href={"/"} style={styles.buttonText}>Voltar</Link>
-            </View>
-        </View>
-    );
+          )}
+        />
+      )}
+
+      <TouchableOpacity
+        style={[styles.button, !isValid ? { backgroundColor: "#888" } : {}]}
+        onPress={handleSubmit(
+          step === 1
+            ? handleEmailSubmit
+            : step === 2
+            ? handleCodeSubmit
+            : handlePasswordSubmit
+        )}
+        disabled={!isValid}
+      >
+        <Text style={styles.buttonText}>{step === 3 ? "Atualizar Senha" : "Avançar"}</Text>
+      </TouchableOpacity>
+
+      {step > 1 && (
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => setStep(step - 1)}
+        >
+          <Text style={styles.backButtonText}>Voltar</Text>
+        </TouchableOpacity>
+      )}
+      {
+        step === 1 && (
+          <View style={styles.backButton}>
+            <Link href={"/"} style={styles.backButtonText}>Voltar para o login</Link>
+          </View>
+        )
+      }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    logoContainer: {
-        flex: 1,
-        backgroundColor: "#121212",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 20,
-    },
-    inputContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        backgroundColor: "#1E1E1E",
-        borderRadius: 8,
-        marginBottom: 10,
-        paddingHorizontal: 10,
-    },
-    inputIcon: {
-        marginRight: 10,
-        fontSize: 20,
-        color: "#888",
-    },
-    input: {
-        flex: 1,
-        color: "#fff",
-        padding: 15,
-        fontSize: 16,
-    },
-    button: {
-        width: "100%",
-        backgroundColor: "#6200EE",
-        padding: 15,
-        borderRadius: 8,
-        alignItems: "center",
-        marginTop: 10,
-    },
-    buttonText: {
-        color: "#fff",
-        fontSize: 18,
-        fontWeight: "bold",
-    },
-    linksContainer: {
-        flexDirection: "row",
-        justifyContent: "flex-end",
-        width: "100%",
-        marginTop: 20,
-    },
-    link: {
-        color: "#BB86FC",
-        fontSize: 14,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#6200EE",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  backButton: {
+    marginTop: 10,
+    padding: 10,
+    alignItems: "center",
+  },
+  backButtonText: {
+    color: "#BB86FC",
+    fontSize: 16,
+  },
 });
