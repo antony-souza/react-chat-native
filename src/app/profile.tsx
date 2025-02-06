48e51b676039e8da0c950f372cc9dbaf9608d40d
antony-souza <antonygustavo09@hotmail.com>
2025-02-06 15:48:59 -0300

    Criando page de perfil

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,23 +1,49 @@
-import { Text, View, Image, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet, Alert } from "react-native";
 import LayoutPage from "../../layouts/dark-layout";
+import { useEffect, useState } from "react";
+import { environment } from "../environment/environment";
+import { httpClient } from "../../utils/generic-request";
+import * as SecureStore from "expo-secure-store";
+
+interface IUserResponse {
+    name: string;
+    email: string;
+    imgUrl: string;
+}
 
 const Profile = () => {
     const title = "Profile";
+    const [user, setUser] = useState<IUserResponse>();
+    const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        fetchUser();
+    }, []);
 
-
-    const userExemple = {
-        name: "Antony Souza",
-        email: "antony@example.com",
-        avatar: "https://i.pravatar.cc/150?img=3"
-    };
+    const fetchUser = async () => {
+        try {
+            setLoading(true);
+            const userId = await SecureStore.getItemAsync("userId");
+            const response = await httpClient.genericRequest(`${environment.getUser}/${userId}`, "GET") as IUserResponse;
+            setUser(response);
+        } catch (error) {
+            Alert.alert("Falha ao buscar as informações de perfil");
+            setLoading(false);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <LayoutPage headerTitle={title} tabs={true}>
             <View style={styles.container}>
-                <Image source={{ uri: userExemple.avatar }} style={styles.avatar} />
-                <Text style={styles.name}>{userExemple.name}</Text>
-                <Text style={styles.email}>{userExemple.email}</Text>
+                <Image
+                    source={{ uri: user?.imgUrl }}
+                    style={styles.avatar}
+                />
+                <Text style={styles.name}>{user?.name}</Text>
+                <Text style={styles.email}>{user?.email}</Text>
             </View>
         </LayoutPage>
     );
