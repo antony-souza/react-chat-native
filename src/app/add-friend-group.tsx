8df2d7059a90a8a48b568780a8332fa68a7d2f78
antony-souza <antonygustavo09@hotmail.com>
2025-02-17 12:05:57 -0300

    Adicionando membro ao grupo e também removendo

---
+++ src/app/add-friend-group.tsx
@@ -0,0 +1,125 @@
+import { FlatList, Text, View, TouchableOpacity, Image, ActivityIndicator, StyleSheet, Alert } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import LayoutPage from "../../layouts/dark-layout";
+import React, { useEffect, useState } from "react";
+import { httpClient } from "../../utils/generic-request";
+import * as Security from "expo-secure-store";
+import { environment } from "../environment/environment";
+import { useGlobalSearchParams, useRouter } from "expo-router";
+
+interface IUserResponse {
+    id: string;
+    userId: string;
+    name: string;
+    image: string;
+}
+
+const AddFriendToGroup = () => {
+    const title = "Adicionando Amigos ao Grupo";
+    const router = useRouter();
+    const { groupPrivateId } = useGlobalSearchParams() as { groupPrivateId: string };
+    const [users, setUsers] = useState<IUserResponse[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // Busca a lista de amigos quando o componente é montado
+        const fetchFriends = async () => {
+            setLoading(true);
+            try {
+                const userId = await Security.getItemAsync("userId");
+                const response = await httpClient.genericRequest(`${environment.findAllFriends}/${userId}`, "GET") as IUserResponse[];
+                setUsers(response);
+            } catch {
+                Alert.alert("Erro ao buscar amigos", "Tente novamente mais tarde");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchFriends();
+    }, []);
+
+    const addFriendToGroup = async (friendId: string) => {
+        try {
+            setLoading(true);
+
+            const response = await httpClient.genericRequest(`${environment.joinChat}/${groupPrivateId}/${friendId}`, "PUT");
+            if (response) {
+                Alert.alert(
+                    "Adicionando ao grupo",
+                    "Amigo adicionado com sucesso!"
+                );
+            }
+        } catch {
+            Alert.alert("Erro ao adicionar amigo ao grupo", "Tente novamente mais tarde");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <LayoutPage headerTitle={title} tabs={true}>
+            <View style={styles.container}>
+                {loading && <ActivityIndicator size="large" color="#007bff" style={styles.loader} />}
+
+                <FlatList
+                    data={users}
+                    keyExtractor={(item) => item.id}
+                    style={{ marginTop: 20 }}
+                    renderItem={({ item }) => (
+                        <View style={styles.card}>
+                            <Image source={{ uri: item.image }} style={styles.userImage} />
+                            <View style={styles.userInfo}>
+                                <Text style={styles.userName}>{item.name}</Text>
+                            </View>
+                            <TouchableOpacity style={styles.addButton} onPress={() => addFriendToGroup(item.userId)}>
+                                <Icon name="user-plus" size={24} color="#fff" />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                />
+            </View>
+        </LayoutPage>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+    },
+    loader: {
+        marginTop: 10,
+    },
+    card: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#222",
+        padding: 15,
+        borderRadius: 10,
+        marginVertical: 8,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 3,
+    },
+    userImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 15,
+    },
+    userInfo: {
+        flex: 1,
+    },
+    userName: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    addButton: {
+        padding: 8,
+        backgroundColor: "#6200EE",
+        borderRadius: 8,
+    },
+});
+
+export default AddFriendToGroup;
--- src/app/information-chat.tsx
+++ src/app/information-chat.tsx
@@ -1,15 +1,17 @@
-import { Text, TouchableOpacity, View, FlatList, StyleSheet, Image } from "react-native";
+import { Text, TouchableOpacity, View, FlatList, StyleSheet, Image, Alert } from "react-native";
 import LayoutPage from "../../layouts/dark-layout";
 import { useEffect, useState } from "react";
 import { httpClient } from "../../utils/generic-request";
 import { environment } from "../environment/environment";
-import { useGlobalSearchParams } from "expo-router";
-
+import { useGlobalSearchParams, useRouter } from "expo-router";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import * as Security from "expo-secure-store";
 
 interface IChatResponse {
     userId: string;
     userName: string;
     userImgUrl: string;
+    userEmail: string;
     chatId: string;
     chatName: string;
     chatImgUrl: string;
@@ -18,6 +20,7 @@ interface IChatResponse {
 const InformationChat = () => {
     const title = "Informações do Grupo";
     const { groupId } = useGlobalSearchParams() as { groupId: string };
+    const router = useRouter()
     const [chat, setChat] = useState<IChatResponse[]>([]);
 
     useEffect(() => {
@@ -29,10 +32,50 @@ const InformationChat = () => {
         setChat(response);
     };
 
+    const handleRouterToAddMembers = () => {
+        router.navigate({
+            pathname: "add-friend-group",
+            params: { groupPrivateId: groupId }
+        });
+    }
+
+    const removeMember = async (userId: string) => {
+        const adminId = await Security.getItemAsync('userId');
+        
+        const response = await httpClient.genericRequest(`${environment.removeUserFromChat}/${groupId}/${userId}/${adminId}`, "PUT");
+
+        if (response) {
+            findChatInfo();
+        }
+    }
+
+    const confirmRemoveUser = (userId: string) => {
+        Alert.alert(
+            "Remover Membro",
+            "Tem certeza que deseja remover esse membro?",
+            [
+                {
+                    text: "Não",
+                    style: "cancel"
+                },
+                {
+                    text: "Sim",
+                    onPress: () => removeMember(userId)
+                }
+            ]
+        );
+    }
+
     const renderMembers = ({ item }: { item: IChatResponse }) => (
         <View style={styles.arrayUsers}>
             <Image source={{ uri: item.userImgUrl }} style={styles.imgUser} />
-            <Text style={styles.userName}>{item.userName}</Text>
+            <View style={styles.userInfo}>
+                <Text style={styles.userName}>{item.userName}</Text>
+                <Text style={styles.userEmail}>{item.userEmail}</Text>
+            </View>
+            <TouchableOpacity onPress={() => confirmRemoveUser(item.userId)} style={styles.removeIcon}>
+                <Icon name="user-times" size={20} color="red" />
+            </TouchableOpacity>
         </View>
     );
 
@@ -43,12 +86,18 @@ const InformationChat = () => {
                     <Image source={{ uri: chat[0]?.chatImgUrl }} style={styles.imageChat} />
                     <Text style={styles.name}>{chat[0]?.chatName}</Text>
                 </View>
-                <Text style={styles.sectionTitle}>Membros</Text>
+                <View style={styles.titleAndAddMembers}>
+                    <Text style={styles.sectionTitle}>Membros</Text>
+                    <TouchableOpacity onPress={handleRouterToAddMembers}>
+                        <Text style={styles.addMembers}>Adicionar Membros</Text>
+                    </TouchableOpacity>
+                </View>
                 <FlatList
                     data={chat}
                     renderItem={renderMembers}
                     keyExtractor={item => item.userId}
                     style={styles.flatlistContainer}
+                    ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
                 />
             </View>
         </LayoutPage>
@@ -77,34 +126,52 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
     },
+    titleAndAddMembers: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
     sectionTitle: {
         color: '#fff',
         fontSize: 20,
         fontWeight: 'bold',
-        marginTop: 10,
+    },
+    addMembers: {
+        color: '#6200EE',
+        fontSize: 16,
     },
     flatlistContainer: {
-        gap: 1,
+        marginTop: 10,
     },
     arrayUsers: {
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: '#1E1E1E',
-        padding: 10,
+        padding: 15,
         gap: 10,
         borderRadius: 10,
         elevation: 2,
+    },
+    userInfo: {
+        flex: 1,
     },
     userName: {
         fontSize: 16,
         fontWeight: '500',
         color: 'white',
     },
+    userEmail: {
+        fontSize: 14,
+        color: '#aaa',
+    },
     imgUser: {
         width: 50,
         height: 50,
         borderRadius: 25,
-    }
+    },
+    removeIcon: {
+        marginLeft: 'auto',
+    },
 });
 
 export default InformationChat;
